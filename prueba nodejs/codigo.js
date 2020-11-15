cca802bfb9a5baef0cef14071c2d98b842aeba68
Tomas Tonina <51637537+tomastonina@users.noreply.github.com>
2020-11-15 14:55:03 -0300

    v15.11.20
    Agregado de las base de datos mas el formulario web.

---
+++ prueba nodejs/codigo.js
@@ -0,0 +1,13 @@
+//var dias = ["lunes", "martes", "miercoles", "jueves", "viernes"] // array / vector
+//console.log(dias[3]);
+
+//let contacto = {"nombre" : "tomas", "email":"tomastonina@gmail.com", "edad":21} // objetos
+//console.log(contacto.nombre);
+
+var colors = require('colors'); // Uso del paquete colors, que cambia el color de lo mostrado en la consola
+//let contacto = {"nombre" : "tomas", "email":"tomastonina@gmail.com", "edad":21} // objetos
+//console.log(contacto.email.green);
+
+var prueba = require('./funciones/funcion1');
+prueba.suma(5,9);
+prueba.resta(10,5);
---
+++ prueba nodejs/funciones/funcion1.js
@@ -0,0 +1,13 @@
+var prueba = {
+    suma : function(a, b){
+        c=a+b;
+        console.log(c);
+    },
+
+    resta : function(a, b){
+        c=a-b;
+        console.log(c);
+    }
+}
+
+module.exports = prueba;
