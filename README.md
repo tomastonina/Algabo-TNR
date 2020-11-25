31711c956723b5fc8e03822708f232fad03ef4e3
Tomas Tonina <51637537+tomastonina@users.noreply.github.com>
2020-11-25 20:46:17 -0300

    v25.11.20
    Descripcion en el README agregada.
    Eliminado el anterior formulario y empezando uno nuevo. Realizando la conexion del formulario a la base de datos.

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Algabo-TNR
  Repositorio creado para los trabajos de Algabo
+
+
+
+Modulos utilizados para la conexion de la base de datos al formulario:
+
+-express 		(framework para realizar el backend)
+-express-handlebars 	(motor de plantillas)
+-express-session 	(administrar las sesiones de nuestra aplicacion, nos sirve para autenticar el usuario)
+-mysql 			(Nos permite conectarnos a la base de datos y hacer consultas)
+-express-mysql-session  (En vez de guardar la sesion en el servidor la guardamos en la base de datos)
+-morgan 		(permite crear mensajes de los clientes a el servidor)
+-bcryptjs 		(Permite cifrar las contraseñas antes de guardarla a la base de datos)
+-passport 		(Permite autenticar y manejar el proceso del login)
+-passport-local
+-timeago.js 		(Convierte las fechas de una base de datos en un formato de 2 minutes ago, o sea para las vistas)
+-connect-flash 		(Permite mostrar mensajes de error o exito dependiendo de la operacion)
+-express-validator 	(validar los datos que el usuario nos envia desde la aplicacion cliente)
+-nodemon		(Podemos hacer cambios en el codigo y verlos en tiempo real)
+
+
---
+++ Web/Log-in/recursos/index.js
@@ -0,0 +1,43 @@
+// A la hora de ejecutar el programa con npm run "exe" (el nombre que yo le puse a un comando) lo que hace es buscar la carpeta recursos
+// el primer archivo index.js que ve, en cambio si tiene otro nombre hay que especificarlo en el package.json
+const express = require('express');
+const morgan = require('morgan');
+const expresshbs = require('express-handlebars');
+const path = require('path');
+
+//Inicializamos todo
+
+const app = express();
+
+//Configuraciones del servidor
+
+app.set('port', 5000);
+app.set('vistas', path.join(__dirname, 'vistas'));
+app.engine('.hbs',expresshbs({
+    defaultLayout: 'principal',
+    layoutsDir: path.join(app.get('vistas'), 'plantillas'),
+    partialsDir: path.join(app.get('vistas'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./libreria/hanblebars')
+}));
+app.set('view engine', '.hbs');
+
+//Peticiones al servidor
+
+app.use(morgan('dev'));
+app.use(express.urlencoded())
+
+//Variables Globales
+
+//Rutas
+
+app.use(require ('./rutas/index-rutas'));
+
+//Archivos Publicos
+
+//Iniciar el Servidor
+
+app.listen(app.get('port'), () => {
+    console.log('El servidor se encuentra en el puerto', app.get('port'));
+});
+
